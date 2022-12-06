57d870357f2a3aeda5ece41a0fe32a5a2aa7123a
Barrokgl <barrokgl@gmail.com>
2022-12-06 09:24:03 +0300

    day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -16,7 +16,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let window_size: u32 = 14;
+    let chars = input.chars().collect::<Vec<char>>();
+
+    let uniq_seq = chars
+        .windows(window_size.try_into().unwrap())
+        .find(|window| HashSet::<char>::from_iter((window).iter().cloned()).len() == window.len())
+        .map(|chars| chars.iter().collect::<String>());
+
+    let uninq_seq_index = input
+        .match_indices(&uniq_seq.unwrap())
+        .find_map(|m| Some(m.0 as u32));
+    uninq_seq_index.map(|n| n + window_size)
 }
 
 fn main() {
