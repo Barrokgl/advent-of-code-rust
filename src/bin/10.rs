897847fdc3dcf9478ec717574547cc5abf6a15c6
Barrokgl <barrokgl@gmail.com>
2022-12-10 12:26:59 +0300

    day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -3,6 +3,7 @@ enum Command {
     Addx(i32),
     Noop,
 }
+use std::iter;
 
 fn parse_command(line: &str) -> Command {
     match line {
@@ -24,13 +25,11 @@ pub fn part_one(input: &str) -> Option<u32> {
             Command::Addx(_) => vec![acc, vec![Command::Noop, cmd]].concat(),
             Command::Noop => vec![acc, vec![cmd]].concat(),
         });
-    println!("commands: {:?}", commands);
     let mut x: i32 = 1;
     let mut result: u32 = 0;
     let cycles_probe = vec![20, 60, 100, 140, 180, 220];
     for (i, cmd) in commands.iter().enumerate() {
         let cycle_number = i + 1;
-        println!("before cycle n {}, x: {}", cycle_number, x);
 
         if cycles_probe.contains(&cycle_number) {
             let singal_power: u32 = (x * cycle_number as i32).try_into().unwrap_or(0);
@@ -41,13 +40,38 @@ pub fn part_one(input: &str) -> Option<u32> {
             Command::Addx(n) => x = x + n,
             Command::Noop => (),
         };
-        println!("after cycle n {}, x: {}", cycle_number, x);
     }
 
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<String> {
+    let commands: Vec<Command> = input
+        .lines()
+        .map(parse_command)
+        .fold(Vec::new(), |acc, cmd| match cmd {
+            Command::Addx(_) => vec![acc, vec![Command::Noop, cmd]].concat(),
+            Command::Noop => vec![acc, vec![cmd]].concat(),
+        });
+    let mut res: Vec<String> = Vec::new();
+    let mut x: i32 = 1;
+
+    for (i, cmds_list) in commands.chunks(40).enumerate() {
+        let mut crt_row: Vec<&str> = iter::repeat(".").take(40).collect();
+        for (pixel, cmd) in cmds_list.iter().enumerate() {
+            if (x - pixel as i32).abs() <= 1 {
+                crt_row[pixel] = "#";
+            }
+
+            match cmd {
+                Command::Addx(n) => x = x + n,
+                Command::Noop => (),
+            };
+        }
+        res.push(crt_row.join(""));
+    }
+    println!("{}", res.join("\n"));
+
     None
 }
 
